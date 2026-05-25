ac72f4783112551eac99ccf29e384bee8c91b4b1
KingBobSkibidi <lucianbai0808@gmail.com>
2026-05-25 11:55:54 -0400

    May 25: animated grandma
    - replaced grandma design
    - gave grandma walking animation, turning left and right according to user input

--- Grandma.java
+++ Grandma.java
@@ -8,27 +8,104 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Grandma extends Actor
 {
+    GreenfootImage[] grandmaRight = new GreenfootImage[4];
+    GreenfootImage[] grandmaLeft = new GreenfootImage[4];
+    
+    String facing = "right";
+    
+    SimpleTimer animationTimer = new SimpleTimer();
+    
+    int imageIndex = 0;
+    
     public Grandma()
     {
-        GreenfootImage level1 = new GreenfootImage("grandma.jpg");
-        level1.scale(150, 150);
-        setImage(level1);
+        for(int i = 0; i < grandmaRight.length; i++)
+        {
+            grandmaRight[i] = new GreenfootImage("grandma_walk/grandma_walk" + (i+1) + ".png");
+            grandmaRight[i].scale(150, 150);
+        }
+        
+        for(int i = 0; i < grandmaLeft.length; i++)
+        {
+            grandmaLeft[i] = new GreenfootImage("grandma_walk/grandma_walk" + (i+1) + ".png");
+            grandmaLeft[i].mirrorHorizontally();
+            grandmaLeft[i].scale(150, 150);
+        }
+        
+        setImage(grandmaRight[0]);
+        animationTimer.mark();
+    }
+    
+    public void animateGrandma()
+    {
+        if(animationTimer.millisElapsed() < 120)
+        {
+            return;
+        }
+        
+        animationTimer.mark();
+        
+        if(facing.equals("right"))
+        {
+            setImage(grandmaRight[imageIndex]);
+        }
+        else
+        {
+            setImage(grandmaLeft[imageIndex]);
+        }
+        
+        imageIndex = (imageIndex + 1) % grandmaRight.length;
     }
     
     public void act()
     {
+<<<<<<< Updated upstream
         /**
         if(Greenfoot.isKeyDown("left")){
+=======
+        boolean moving = false;
+        
+        if(Greenfoot.isKeyDown("left"))
+        {
+>>>>>>> Stashed changes
             setLocation(getX() - 3, getY());
+            facing = "left";
+            moving = true;
         }
-        else if(Greenfoot.isKeyDown("right")){
+        
+        if(Greenfoot.isKeyDown("right"))
+        {
             setLocation(getX() + 3, getY());
+            facing = "right";
+            moving = true;
         }
-        else if(Greenfoot.isKeyDown("up")){
+        
+        if(Greenfoot.isKeyDown("up"))
+        {
             setLocation(getX(), getY() - 3);
+            moving = true;
         }
-        else if(Greenfoot.isKeyDown("down")){
+        
+        if(Greenfoot.isKeyDown("down"))
+        {
             setLocation(getX(), getY() + 3);
+            moving = true;
+        }
+        
+        if(moving)
+        {
+            animateGrandma();
+        }
+        else
+        {
+            if(facing.equals("right"))
+            {
+                setImage(grandmaRight[0]);
+            }
+            else
+            {
+                setImage(grandmaLeft[0]);
+            }
         }
         */
     }
